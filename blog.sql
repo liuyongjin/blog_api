3a2d025c316e262b4234123cd491141d5369cd14
liuyongjin <572551431@qq.com>
2019-01-07 20:12:10 +0800

    sql

---
+++ blog.sql
@@ -0,0 +1,170 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2019-01-07 20:11:34
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "article"
+#
+
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(100) NOT NULL DEFAULT '',
+  `main_img` varchar(255) NOT NULL DEFAULT '',
+  `content` longtext NOT NULL,
+  `comment_count` int(11) unsigned NOT NULL DEFAULT '0',
+  `praise_count` int(11) unsigned NOT NULL DEFAULT '0',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+#
+# Data for table "article"
+#
+
+
+#
+# Structure for table "comment"
+#
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `comment` varchar(255) NOT NULL DEFAULT '',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+#
+# Data for table "comment"
+#
+
+
+#
+# Structure for table "config"
+#
+
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE `config` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `config_dev` int(11) unsigned NOT NULL DEFAULT '0',
+  `config_name` varchar(50) NOT NULL DEFAULT '',
+  `config_key` varchar(100) NOT NULL DEFAULT '',
+  `config_value` varchar(100) NOT NULL DEFAULT '',
+  `config_ext` text NOT NULL,
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+#
+# Data for table "config"
+#
+
+/*!40000 ALTER TABLE `config` DISABLE KEYS */;
+/*!40000 ALTER TABLE `config` ENABLE KEYS */;
+
+#
+# Structure for table "images"
+#
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `url` varchar(255) NOT NULL DEFAULT '',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+#
+# Data for table "images"
+#
+
+
+#
+# Structure for table "login_log"
+#
+
+DROP TABLE IF EXISTS `login_log`;
+CREATE TABLE `login_log` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_name` varchar(255) NOT NULL DEFAULT '',
+  `login_ip` char(20) NOT NULL DEFAULT '',
+  `login_adress` varchar(255) NOT NULL DEFAULT '',
+  `user_or_memeber` bit(1) NOT NULL DEFAULT b'0' COMMENT '后台用户还是前台用户,0前台，1后台',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='登录日志';
+
+#
+# Data for table "login_log"
+#
+
+
+#
+# Structure for table "member"
+#
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `token` varchar(255) NOT NULL DEFAULT '',
+  `avatar` varchar(100) NOT NULL DEFAULT '',
+  `last_login_ip` varchar(20) NOT NULL DEFAULT '',
+  `last_login_time` int(10) NOT NULL DEFAULT '0',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='后台用户表';
+
+#
+# Data for table "member"
+#
+
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL DEFAULT '',
+  `nickname` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `token` varchar(255) NOT NULL DEFAULT '',
+  `avatar` varchar(100) NOT NULL DEFAULT '',
+  `last_login_ip` varchar(20) NOT NULL DEFAULT '',
+  `last_login_time` int(10) NOT NULL DEFAULT '0',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `delete_time` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COMMENT='后台用户表';
+
+#
+# Data for table "user"
+#
+
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
