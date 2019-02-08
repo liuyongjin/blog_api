90ccd3f857c7253812f3134dd8fe535bfffecda4
liuyongjin <572551431@qq.com>
2019-02-08 14:03:38 +0800

    增加跨域

---
+++ application/api/behavior/CORS.php
@@ -0,0 +1,18 @@
+<?php
+// tp5.1的写法
+namespace app\api\behavior;
+
+use think\Request;
+
+class CORS
+{
+    public function appInit($params)
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: POST,GET');
+        if(request()->isOptions()){
+            exit();
+        }
+    }
+}
