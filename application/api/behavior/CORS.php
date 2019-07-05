ef2ee3dcf638b33e29e242727219ed77c5708e10
liuyongjin <572551431@qq.com>
2019-07-05 15:41:29 +0800

    测试

--- application/api/behavior/CORS.php
+++ application/api/behavior/CORS.php
@@ -14,7 +14,7 @@ class CORS
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
         header('Access-Control-Allow-Methods: POST,GET');
         if(request()->isOptions()){
-            exit();
+            exit(123);
         }
     }
 }
