d13208d161515a097959f3b3cf176ecd7f9079b7
liuyongjin <572551431@qq.com>
2019-07-05 23:51:06 +0800

    修改文章接口

--- application/api/model/Article.php
+++ application/api/model/Article.php
@@ -99,6 +99,7 @@ class Article extends BaseModel
     public static function getArticle($data)
     {
         $resQuery=static::with('tags');
+        $where=['status'=>1];
         $countQuery=new Article();
         //各种条件筛选
         if(isset($data['tags_id']) && count($data['tags_id'])>0){
@@ -115,8 +116,8 @@ class Article extends BaseModel
         }
         if(isset($data['status'])){
             $where=['status'=>$data['status']];
-            $resQuery=$resQuery->where($where);
-            $countQuery = $countQuery->where($where);
+            // $resQuery=$resQuery->where($where);
+            // $countQuery = $countQuery->where($where);
         }
         if(isset($data['create_date'])){
             if(isset($data['tags_id']) && count($data['tags_id'])>0){
@@ -136,10 +137,10 @@ class Article extends BaseModel
             }
             $resQuery=$resQuery->order($order[0],$order[1]);
         }
-        $article=$resQuery->limit($data['pageSize'])->page($data['current'])->fetchSql(false)->select();
+        $article=$resQuery->where($where)->limit($data['pageSize'])->page($data['current'])->fetchSql(false)->select();
         // var_dump($article);
         // exit;
-        $count=$countQuery->count();
+        $count=$countQuery->where($where)->count();
         if(!$article){
             throw new BaseException(
             [
