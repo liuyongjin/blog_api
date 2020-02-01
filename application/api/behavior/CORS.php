23630f061b88604fa01fd80b51f273ffaf8b4cd4
liuyongjin <572551431@qq.com>
2020-02-01 12:34:49 +0800

    修改跨域

--- application/api/behavior/CORS.php
+++ application/api/behavior/CORS.php
@@ -8,7 +8,7 @@ class CORS
 {
     public function appInit($params)
     {
-        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: https://envelops.liuyongjin.cn');
         header('Content-type: application/json');
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
         header('Access-Control-Allow-Methods: POST,GET');
