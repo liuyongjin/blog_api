d4fbb52fbd80bcf60adce08e22feb2c875be2d81
liuyongjin <572551431@qq.com>
2020-03-01 20:05:17 +0800

    feat:新增Index接口测试数据

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,6 +5,55 @@ class Index
 {
     public function index()
     {
-        return 'hello world!!!!!!';
+        return [
+            {
+                'title': '学习笔记学习笔记学习笔记学习笔记学习笔记学习笔记学习笔记学习笔记',
+                'content': '学习内容学习内容学习内容学习内容学习内容学习内容学习内容学习内容学习内容学习内容学习内容学习内容学习内容',
+                'image': 'images/yasuo5.png',
+                'browse': '0',
+                'comment': '0',
+                'time': '2020/2/6',
+            },
+            {
+                'title': '学习笔记',
+                'content': '学习内容',
+                'image': 'images/yasuo5.png',
+                'browse': '0',
+                'comment': '0',
+                'time': '2020/2/6',
+            },
+            {
+                'title': '学习笔记',
+                'content': '学习内容',
+                'image': 'images/yasuo5.png',
+                'browse': '0',
+                'comment': '0',
+                'time': '2020/2/6',
+            },
+            {
+                'title': '学习笔记',
+                'content': '学习内容',
+                'image': 'images/yasuo5.png',
+                'browse': '0',
+                'comment': '0',
+                'time': '2020/2/6',
+            },
+            {
+                'title': '学习笔记',
+                'content': '学习内容',
+                'image': 'images/yasuo5.png',
+                'browse': '0',
+                'comment': '0',
+                'time': '2020/2/6',
+            },
+            {
+                'title': '学习笔记',
+                'content': '学习内容',
+                'image': 'images/yasuo5.png',
+                'browse': '0',
+                'comment': '0',
+                'time': '2020/2/6',
+            },
+        ];
     }
 }
