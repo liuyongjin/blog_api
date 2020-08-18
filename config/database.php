449ca2f3d3f5370525bec5fc819981580110e031
liuyongjin <572551431@qq.com>
2020-08-18 22:45:29 +0800

    数据库

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '',
+    'hostname'        => '39.97.209.66',
     // 数据库名
-    'database'        => '',
+    'database'        => 'blog',
     // 用户名
-    'username'        => '',
+    'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'liu7350138',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
